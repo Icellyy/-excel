f0e8a3ccf39b52ba8211fe62c5e4364fc89f8826
Icellyy <154350442+Icellyy@users.noreply.github.com>
2024-02-06 21:13:50 +0200

    Add files via upload

---
+++ Компьютерный практикум 16 (31.01.2024 г.)/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Компьютерный_практикум_16__31._01._2024_г._
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // 1.Вывести в строку десять заглавных букв "а" через один пробел:
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.Write("А ");
+            //}
+            //Console.ReadKey();
+
+
+            //2.Вывести в обратном порядке числа от 50 до 40.
+            //for (int i = 50; i >= 40; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadKey();
+
+
+            // 3.Вывести в строку через пробел следующие натуральные числа: 6    12     24      30      36
+            //for (int i = 6; i <= 36; i += 6)
+            //    if (i != 18)
+            //    {
+            //        Console.Write($"{i} ");
+            //}       
+            //Console.ReadKey();
+
+            // 4.Вывести двузначные числа, кратные 11, подсчитать их количество и сумму.
+            //int summa = 0;
+            //int quantity = 0;
+            //for (int i = 10; i <= 99; i++)
+            //    if (i % 11 == 0)
+            //    {
+            //        Console.WriteLine(i);                 
+            //        quantity++;
+            //        summa += i;
+            //    }
+            //Console.WriteLine($"Кол-во = {quantity}\nСумма = {summa}");
+            //Console.ReadKey();  
+        }
+    }
+}
---
+++ Компьютерный практикум 16 (31.01.2024 г.)/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Компьютерный практикум 16 (31.01.2024 г.)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Компьютерный практикум 16 (31.01.2024 г.)")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("00d724ac-baa0-496b-9062-e96c39b24503")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
